48cc35a4d51a4ecb8c209ca75d2b9509b5daf3cd
Panda <ausazhnev@gmail.com>
2025-03-28 18:34:29 +0300

    ADD: Тесты для интефейса CarList

---
+++ src/test/java/CarListTest.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarListTest {
+
+    private CarList carList;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        for (int i = 0; i < 100; i++) {
+            carList.add(new Car("Brand" + i, i));
+        }
+    }
+
+    @Test
+    public void whenAdd100ElementsThenSizeMustBe100() {
+        assertEquals(100, carList.size());
+    }
+
+    @Test
+    public void whenElementRemoveByIndexThenSizeMustBuDecreased() {
+        assertTrue(carList.removeAt(0));
+        assertEquals(99, carList.size());
+    }
+
+    @Test
+    public void whenElementRemoveThenSizeMustBuDecreased() {
+        Car car = new Car("Mazda", 42);
+        assertEquals(101, carList.size());
+        assertTrue(carList.remove(car));
+        assertEquals(100, carList.size());
+    }
+
+    @Test
+    public void whenNonExistentElementRemovedThenReturnFalse() {
+        Car car = new Car("Mazda", 42);
+        assertFalse(carList.remove(car));
+        assertEquals(100, carList.size());
+
+    }
+
+    public void whenListClearedThenSizeMustBe0() {
+        carList.clear();
+        assertEquals(0, carList.size());
+    }
+
+    @Test
+    public void whenIndexOutOfBounbsThenThrownException() {
+        assertThrows(IndexOutOfBoundsException.class, () -> carList.get(100));
+    }
+
+    @Test
+    public void methodGetReturmedRightValue() {
+        Car car = carList.get(0);
+        assertEquals("Brand0", car.getBrand());
+    }
+}
