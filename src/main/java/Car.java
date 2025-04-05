00c4314a70d76e9e6976ec44b0e6db49b5bb4654
Panda <ausazhnev@gmail.com>
2025-04-05 21:16:19 +0300

    UPDATE: добавлено переопределение методов equals и hashCode средствами IDEA

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Car {
     private String brand;
     private int number;
@@ -15,18 +17,33 @@ public class Car {
         return number;
     }
 
+    // Своя реализация метода equals
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (obj instanceof Car) {
+//            Car car = (Car) obj;
+//            return this.brand.equals(car.brand) && this.number == car.number;
+//        } else {
+//            return false;
+//        }
+//    }
+
+    // Своя реализация метода hashCode
+//    @Override
+//    public int hashCode() {
+//        return this.brand.hashCode() + this.number;
+//    }
+
+
     @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Car) {
-            Car car = (Car) obj;
-            return this.brand.equals(car.brand) && this.number == car.number;
-        } else {
-            return false;
-        }
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return number == car.number && Objects.equals(brand, car.brand);
     }
 
     @Override
     public int hashCode() {
-        return this.brand.hashCode() + this.number;
+        return Objects.hash(brand, number);
     }
 }
