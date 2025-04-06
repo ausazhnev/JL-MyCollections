6db01c76299b9221a4fb751be736ddf3759e0095
Panda <ausazhnev@gmail.com>
2025-04-06 12:10:10 +0300

    UPDATE: Добавлена инициализация

--- src/test/java/CarSetTest.java
+++ src/test/java/CarSetTest.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarSetTest {
-    private CarSet carSet;
+    private CarHashSet carSet;
 
     @BeforeEach
     void setUp() {
-        // carSet = new CarSet();
+        carSet = new CarHashSet();
         for (int i = 0; i < 100; i++) {
             carSet.add(new Car("Brand" + i, i));
         }
